fb2c7fe61c1e49d6692d3bc73b1e2a7528698273
jchristadore-ux <jchristadore@gmail.com>
2026-07-06 12:40:27 -0400

    Add configuration settings for environment variables

--- config.js
+++ config.js
@@ -1,1 +1,20 @@
+export const CONFIG = {
+  loginUrl: process.env.LOGIN_URL,
+  teeSheetUrl: process.env.TEE_SHEET_URL,
 
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+
+  course: "Minerals",
+
+  players: 3,
+
+  timeWindowStart: "08:00",
+  timeWindowEnd: "09:30",
+
+  retryDurationMs: 5 * 60 * 1000, // 5 minutes
+
+  telegramEnabled: !!process.env.TELEGRAM_BOT_TOKEN,
+  telegramBotToken: process.env.TELEGRAM_BOT_TOKEN,
+  telegramChatId: process.env.TELEGRAM_CHAT_ID
+};
